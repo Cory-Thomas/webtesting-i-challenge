5f3c3e05e6d6e8d00df3023512be7df3a48d1669
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-14 16:00:16 -0400

    met mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,33 @@
 module.exports = {
   success,
   fail,
-  repair,
-  get,
+  repair
 };
 
 function success(item) {
+  if(item.enhancement < 20){
+    item.enhancement = item.enhancement + 1
+    return { ...item };
+  } else {
   return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    item.durability = item.durability - 5 
+    return { ...item}
+  } else if (item.enhancement >= 15 ){
+    item.durability = item.durability - 10
+      if(item.enhancement > 16){
+        item.enhancement = item.enhancement - 1
+        return { ...item}
+      } else {
+        return { ...item}
+      }}
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+  item.durability = 100 
+  return { ...item}
 }
